668b06ea3fca5cd6a73a0fac2e9108b364ce0e74
hiroki.miyamoto <hiroki_12.22_goal@mx5.canvas.ne.jp>
2018-09-25 08:01:28 +0000

    third commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,52 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end 
   
   def show
+    @task = Task.find(params[:id])
   end
   
   def new
+    @task = Task.new
   end
   
   def create 
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'taskが正常に投稿されました'
+    redirect_to @task
+    
+    else
+      flash.now[:danger] = 'taskが投稿されませんでした'
+      
+    render :new
+    end
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
-  end 
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'taskは正常に更新されました'
+    redirect_to @task
+      
+    else
+      flash.now[:danger] = 'taskは更新されませんでした'
+      
+      render :new
+    end 
+  end
+
+private
+
+def task_params
+  params.require(:task).permit(:content)
 end
+end
+
