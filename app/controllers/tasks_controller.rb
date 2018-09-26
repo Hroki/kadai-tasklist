5b2bcfe7d3ea5f0b372b875748b4bdfa85fb4906
hiroki.miyamoto <hiroki_12.22_goal@mx5.canvas.ne.jp>
2018-09-26 14:13:35 +0000

    fifth commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,8 +53,12 @@ class TasksController < ApplicationController
   
 private
 
+def set_task
+  @task = Task.find(params[:id])
+end
+
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
 end
 end
 
