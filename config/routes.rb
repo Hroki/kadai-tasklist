d5e47c989768294d35dc3cc25e2759c0d56cc0ee
hiroki.miyamoto <hiroki_12.22_goal@mx5.canvas.ne.jp>
2018-10-06 08:37:55 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,33 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'microposts/create'
+
+  get 'microposts/destroy'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'toppages/index'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root to: 'toppages#index'
   
-  resources :tasks
+   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
+  get 'signup', to: 'users#new'
+  resources :tasks 
+  resources :users, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
